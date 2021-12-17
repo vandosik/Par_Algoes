60ad0b119187430526d5e9af493edbde0744bf0e
vandosik <dubinin.vania2011@yandex.ru>
2021-12-18 02:04:23 +0300

    Add first task omp code

---
+++ WorkDir/task_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "omp.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    unsigned w_num = 0;
+
+    if (argc < 2)
+    {
+        cout << "No argument passed" << endl;
+        return 1; // Exit with failure
+    }
+
+    char*       string = argv[1];
+    char        new_symb, prev_symb;
+    unsigned    i = 1;
+
+    prev_symb = new_symb = string[0];
+
+    while (new_symb != '\0') // End of line
+    {
+        new_symb = string[i];
+
+        if (!(prev_symb == ' ' || prev_symb == '\t' || prev_symb == '\n') &&
+            (new_symb == ' ' || new_symb == '\t' || new_symb == '\n' || new_symb == '\0'))
+        {
+            w_num++; // Increment words number
+            cout << "Increment:" << prev_symb << " prev:" << new_symb << endl;
+        }
+        prev_symb = new_symb;
+        i++;
+    }
+
+    cout << "Number of words:" << w_num << endl;
+
+    return 0;
+}

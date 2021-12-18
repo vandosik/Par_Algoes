9f18b621070aa86dbb13caf8cbacafe8733ea21a
vandosik <dubinin.vania2011@yandex.ru>
2021-12-19 00:01:07 +0300

    refactoring of task_1.cpp

--- WorkDir/task_1.cpp
+++ WorkDir/task_1.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 #include <omp.h>
 #include <chrono>
 
 using namespace std;
-using namespace std::chrono;
 
-int main(int argc, char** argv){
-    if (argc > 1) // if we pass arguments to function, argc will be more than 1 (depends on number of arguments)
+int main(int argc, char** argv)
+{
+    if (argc > 1) // if we pass arguments to function, argc will be more than 1
     {
-        int vec_size = atoi (argv [1]); // size of the vector
-        double* randArray = new double[vec_size];
-        for (long int i = 0; i < vec_size; i++) { //Generate number between 0 to 1
-            randArray[i] = (double)rand() / RAND_MAX;
-        }
-        //cout<<"\nElements of the array::"<<endl;
-        // printing elements of an array
-        //for(int i = 0; i < vec_size; i++)
-        //    cout<<"Elements no "<< i + 1<< "::" << randArray[i] <<endl;
-        //  }
+        int vec_size = atoi(argv [1]); // size of the vector
 
-        // iterate over the number of threads
-        for(int thread_num = 1; thread_num < 11; thread_num++){
-            double max_val = -1.0; // as all the elements in array between 0 to 1
-            auto start = high_resolution_clock::now(); // get the current time
-            #pragma omp parallel for num_threads(thread_num) reduction (max:max_val)
-            for (int i = 0; i < vec_size; i++){
-                if (randArray[i] > max_val){max_val = randArray[i];}
+        vector<double> randArray(vec_size);
+
+        for (auto &vec_item: randArray) // Fill in random variables
+        {
+            vec_item = (double)rand() / RAND_MAX;
+        }
+        // Print elements if pass 2nd arg and our array is not very big
+        if (argc > 2 and vec_size <= 10)
+        {
+            cout<<"Elements of the array: ";
+            for (auto &vec_item: randArray)
+            {
+                cout << vec_item << "  ";
             }
-            auto stop = high_resolution_clock::now(); // get the stop time
-            auto duration = duration_cast<microseconds>(stop - start); // calculate the duration
-            printf("Number of threads is %d, Execution time (in microseconds): %lld\n", thread_num, duration.count());
+            cout << endl;
+        }
+        // iterate over the number of threads
+        for(auto thread_num = 1; thread_num < 11; thread_num++)
+        {
+            double max_val = -1.0; // as all the elements in array between 0 to 1
+
+            auto time_start = chrono::high_resolution_clock::now(); // get the current time
+
+#pragma omp parallel for num_threads(thread_num) reduction (max:max_val)
+            for (auto i = 0; i < vec_size; i++)
+            {
+                if (randArray[i] > max_val)
+                {
+                    max_val = randArray[i];
+
+                }
+            }
+            auto time_stop = chrono::high_resolution_clock::now(); // get the stop time
+
+            auto duration = chrono::duration_cast<chrono::microseconds>(time_stop - time_start); // calculate the duration
+
+            cout << "Number of threads:" << thread_num << " Execution time (in microseconds): " <<  duration.count() << endl;
+
+            // Print elements if pass 2nd arg and our array is not very big
+            if (argc > 2 and vec_size <= 10)
+            {
+                cout<<"Found maxval: " << max_val << endl;
+
+            }
         }
     }
     else

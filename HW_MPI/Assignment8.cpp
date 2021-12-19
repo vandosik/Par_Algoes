0f0029eb7c659cf5135de83db8411f31ea0a30da
vandosik <dubinin.vania2011@yandex.ru>
2021-12-20 02:00:18 +0300

    Removes tabs in task_8

--- HW_MPI/Assignment8.cpp
+++ HW_MPI/Assignment8.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 void send_int_arr(int src, int dest, int size, int *arr, MPI_Status status, int n_times)
 {
-	int L = size * sizeof(int);
-	double start, end, T, R, s;
+    int L = size * sizeof(int);
+    double start, end, T, R, s;
 
-	start = MPI_Wtime();
-	for (int i = 0; i < n_times; i++)
+    start = MPI_Wtime();
+    for (int i = 0; i < n_times; i++)
     {
-		MPI_Send(arr, size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        MPI_Send(arr, size, MPI_INT, dest, 0, MPI_COMM_WORLD);
     }
-	end = MPI_Wtime();
+    end = MPI_Wtime();
 
-	T = (end - start) * 1000000;  // in us
-	if (size > 0)
+    T = (end - start) * 1000000;  // in us
+    if (size > 0)
     {
         // Find Bandwidth
         R = (2 * L * n_times) / (T * 1024 * 1024);  // in MB/s
@@ -37,43 +37,43 @@ void send_int_arr(int src, int dest, int size, int *arr, MPI_Status status, int 
 
 void recv_int_arr(int src, int dest, int size, int *arr, MPI_Status status, int n_times)
 {
-	for (int i = 0; i < n_times; i++)
+    for (int i = 0; i < n_times; i++)
     {
-		MPI_Recv(arr, size, MPI_INT, dest, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(arr, size, MPI_INT, dest, 0, MPI_COMM_WORLD, &status);
     }
 }
 
 int main(int argc, char **argv)
 {
-	// Init MPI
-	int rank, size, n_times = 10;
+    // Init MPI
+    int rank, size, n_times = 10;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Status status;
 
-	for (int message_size = 0; message_size <= 1000000; message_size *= 10)
-	{
-		int *arr = new int[message_size]();
+    for (int message_size = 0; message_size <= 1000000; message_size *= 10)
+    {
+        int *arr = new int[message_size]();
 
-		if (rank == 0)
-		{
-			for (int i = 0; i < message_size; i++)
+        if (rank == 0)
+        {
+            for (int i = 0; i < message_size; i++)
             {
                 arr[i] = 1;
             }
 
-			send_int_arr(0, 1, message_size, arr, status, n_times);
-			recv_int_arr(0, 1, message_size, arr, status, n_times);
-		}
-		else if (rank == 1)
-		{
-			recv_int_arr(1, 0, message_size, arr, status, n_times);
-			send_int_arr(1, 0, message_size, arr, status, n_times);
-		}
+            send_int_arr(0, 1, message_size, arr, status, n_times);
+            recv_int_arr(0, 1, message_size, arr, status, n_times);
+        }
+        else if (rank == 1)
+        {
+            recv_int_arr(1, 0, message_size, arr, status, n_times);
+            send_int_arr(1, 0, message_size, arr, status, n_times);
+        }
 
-		delete arr;
+        delete arr;
 
         if (message_size == 0)
         {
@@ -81,10 +81,10 @@ int main(int argc, char **argv)
         }
         // Wait both procs to reach here
         MPI_Barrier(MPI_COMM_WORLD);
-	}
+    }
 
-	// Finalize MPI
-	MPI_Finalize();
+    // Finalize MPI
+    MPI_Finalize();
 
-	return 0;
+    return 0;
 }

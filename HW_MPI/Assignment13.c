217ffc8475a71952a8dd333a665d2eabfe45b201
vandosik <dubinin.vania2011@yandex.ru>
2021-12-23 04:36:52 +0300

    In Task 13 comments are moved

--- HW_MPI/Assignment13.c
+++ HW_MPI/Assignment13.c
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     int proc_rank, comm_size;
-//matrix initialization by each process
+    //matrix initialization by each process
     int matrix_a[MATRIX_SIZE][MATRIX_SIZE], matrix_b[MATRIX_SIZE][MATRIX_SIZE], matrix_c[MATRIX_SIZE][MATRIX_SIZE];
     double time_start, time_stop;
 
@@ -29,9 +29,9 @@ int main(int argc, char **argv)
     }
     // Wait for all procs here
     MPI_Barrier(MPI_COMM_WORLD);
-// start timing for each process
+    // start timing for each process
     time_start = MPI_Wtime();
-// matrix multiplication
+    // matrix multiplication
     for (int i = 0; i < MATRIX_SIZE; i++)
     {
         for (int j = 0; j < MATRIX_SIZE; j++)
@@ -42,9 +42,9 @@ int main(int argc, char **argv)
             }
         }
     }
-// end of timing
+    // end of timing
     time_stop = MPI_Wtime();
-// output the execution time of matrix multiplication at each process
+    // output the execution time of matrix multiplication at each process
     cout << "Process: " << proc_rank << " counted for " << setw(8) << (time_stop - time_start) * 1000000 << " usec" << endl;
 
     MPI_Finalize();
